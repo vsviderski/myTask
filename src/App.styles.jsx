b541a0d06ebf9d448288c993722cd381b7b004a5
Shkirenkoroma <shkirenkoroma@mail.ru>
2022-11-10 17:23:37 +0300

    add to change

--- src/App.styles.jsx
+++ src/App.styles.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Block = styled.div`
 	width: 1150px;
 	background-color: white;
-	margin-left: 250px;
-	margin-top: 50px;
+	margin:auto;
 	border-radius: 10px;
 `;
